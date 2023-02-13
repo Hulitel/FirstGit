f8ee9853ba98a7fe174e4369fd99b4a0d5c25d7e
Hulitel <Hulitel@proton.me>
2023-02-13 11:23:27 +0200

    Implements TelegramBot class

--- src/main/java/ua/dmjdev/teletarget/service/Bot.java
+++ src/main/java/ua/dmjdev/teletarget/service/Bot.java
@@ -1,5 +1,19 @@
 package ua.dmjdev.teletarget.service;
 
-public class Bot {
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+
+public class Bot implements TelegramBot {
+
+	@Override
+	public String getBotToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBotUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
